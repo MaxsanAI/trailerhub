67aacb0d1b194a4068e192d28ff540fdec06d970
MaxsanAI <maxsansamurai@gmail.com>
2026-04-25 23:04:15 +0200

    Add API function to fetch popular movies

---
+++ functions/api/movies.js
@@ -0,0 +1,15 @@
+export async function onRequest({ env }) {
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${env.TMDB_API_KEY}`
+  );
+
+  const data = await res.json();
+
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    }
+  });
+}
