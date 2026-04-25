e6d0abf9cbfce985361a0eb76e8575c44da21a88
MaxsanAI <maxsansamurai@gmail.com>
2026-04-26 01:27:34 +0200

    Add request parameter to onRequest function

--- functions/api/movies.js
+++ functions/api/movies.js
@@ -1,7 +1,10 @@
-export async function onRequest({ env }) {
+export async function onRequest({ request, env }) {
+  const url = new URL(request.url);
+  const page = url.searchParams.get("page") || 1;
+
   try {
     const res = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${env.TMDB_API_KEY}`
+      `https://api.themoviedb.org/3/movie/popular?api_key=${env.TMDB_API_KEY}&page=${page}`
     );
 
     const data = await res.json();
@@ -14,8 +17,9 @@ export async function onRequest({ env }) {
     });
 
   } catch (err) {
-    return new Response(JSON.stringify({ error: "API failed" }), {
-      status: 500
-    });
+    return new Response(
+      JSON.stringify({ error: "movies failed", details: err.message }),
+      { status: 500 }
+    );
   }
 }
