5c77f4d0f6ff65e24642dddb51ff0616480e83b3
MaxsanAI <maxsansamurai@gmail.com>
2026-04-25 23:24:35 +0200

    Implement API endpoint for movie videos

---
+++ functions/api/movie-videos.js
@@ -0,0 +1,21 @@
+export async function onRequest({ request, env }) {
+  const url = new URL(request.url);
+  const id = url.searchParams.get("id");
+
+  if (!id) {
+    return new Response(JSON.stringify({ error: "missing id" }), { status: 400 });
+  }
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${env.TMDB_API_KEY}`
+  );
+
+  const data = await res.json();
+
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    }
+  });
+}
